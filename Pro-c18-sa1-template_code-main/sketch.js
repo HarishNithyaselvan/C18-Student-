43b390b4f703cdd767d38502fa171a0a9865beb8
HarishNithyaselvan <117954345+HarishNithyaselvan@users.noreply.github.com>
2022-12-14 18:22:13 -0500

    Add files via upload

---
+++ Pro-c18-sa1-template_code-main/sketch.js
@@ -0,0 +1,15 @@
+var s1
+var s2
+function setup() {
+  createCanvas(400,400);
+  s1 = new Student("Jon",13,9)
+  s1.display()
+  s2 = new Student("Mike",8,5)
+  s2.display()
+}
+
+function draw() 
+{
+  background(30);
+  
+}
